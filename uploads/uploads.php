cf76f709e12b13bf31880928e266395d2aef485a
Tiffany E Johnson unknown student <tej2@afsconnect2.njit.edu>
2017-10-14 11:48:39 -0400

    added uploads.php

---
+++ uploads/uploads.php
@@ -0,0 +1,20 @@
+<?php
+$target_dir = "uploads/";
+$target_file = $target_dir . basename($_FILES["fileToUpload"]["name"]);
+$uploadOk = 1;
+$extension = pathinfo($_FILES['csvFileRep']['name'], \PATHINFO_EXTENSION);
+if($info['extension'] == 'csv'){
+ // Good to go
+  }
+  // Check if file already exists
+  if (file_exists($target_file)) {
+      echo "Sorry, file already exists.";
+          $uploadOk = 0;
+	  }
+	   // Check file size
+	   if ($_FILES["fileToUpload"]["size"] > 500000) {
+	       echo "Sorry, your file is too large.";
+	           $uploadOk = 0;
+		   }
+                                                  ?>
+
