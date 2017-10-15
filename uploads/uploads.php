45c32c4d16a2ababee808c90d75aa161dc37957a
Tiffany E Johnson unknown student <tej2@afsconnect2.njit.edu>
2017-10-14 22:15:44 -0400

    edit

--- uploads/uploads.php
+++ uploads/uploads.php
@@ -1,25 +1,17 @@
+<html>
+<head>
+<title>Process Uploaded File</title>
+</head>
+<body>
 <?php
-$target_dir = "uploads/";
-$target_file = $target_dir . basename($_FILES["fileToUpload"]["name"]);
-$uploadOk = 1;
-$extension = pathinfo($_FILES['csvFileRep']['name'], \PATHINFO_EXTENSION);
-if($info['extension'] == 'csv'){
- // Good to go
-  }
-  // Check if file already exists
-  if (file_exists($target_file)) {
-      echo "Sorry, file already exists.";
-          $uploadOk = 0;
-	  }
-	   // Check file size
-	   if ($_FILES["fileToUpload"]["size"] > 500000) {
-	       echo "Sorry, your file is too large.";
-	           $uploadOk = 0;
-		   }
-		   ?>
- <html>
- <?php
+$GET ["filetoupload"];
+$GET ["yourfilename"];
+?>
+      </body>
+       </html>
+       
+<?php
   // This results in an error.
   // The output above is before the header() call
-  header('Location: index.php?page=fileupload&filename=yourfilename');
+  header('Location: index.php?page=filetoupload&filename=yourfilename');
 exit;			   ?>
