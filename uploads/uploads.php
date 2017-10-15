75af2e6098497d64058eb0032eef374ebadae430
Tiffany E Johnson unknown student <tej2@afsconnect2.njit.edu>
2017-10-14 22:28:27 -0400

    Edits

--- uploads/uploads.php
+++ uploads/uploads.php
@@ -1,17 +1,10 @@
-<html>
-<head>
-<title>Process Uploaded File</title>
-</head>
-<body>
-<?php
-$GET ["filetoupload"];
-$GET ["yourfilename"];
-?>
-      </body>
-       </html>
+
        
 <?php
+  $GET ["filetoupload"];
+  $GET ["yourfilename"];
   // This results in an error.
   // The output above is before the header() call
-  header('Location: index.php?page=filetoupload&filename=yourfilename');
+  header('Location index.php?page=filetoupload&filename=yourfilename');
 exit;			   ?>
+
