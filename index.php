d68729ac8d780f94980af51ccefd7765514a92ce
Tiffany E Johnson unknown student <tej2@afsconnect2.njit.edu>
2017-10-14 22:00:30 -0400

    edit

--- index.php
+++ index.php
@@ -1,18 +1,15 @@
 <?php
    if( $_GET["filetoupload"] || $_GET["yourfilename"] ) {
-		           exit();
-			      }
-			      ?>
-<html>
-<body>
+                              exit();
+			                                    }
+							                                  ?>
+											  <html>
+											  <body>
 
-<form action="https://web.njit.edu/~tej2/project1/uploads/uploads.php"
-method="post" enctype="multipart/form-data">
-    Select file  to upload:
-            <input type="file" name="filetoupload" id="filetoupload">
-	            <input type="submit" value="Upload File" name="yourfilename">
-		    </form>
-
-		    </body>
-		    </html>
-
+											  <form
+	 action="https://web.njit.edu/~tej2/project1/uploads/uploads.php"
+											  method="post"										  enctype="multipart/form-data">
+										 Select file to upload:
+ <input type="file" name="filetoupload" id="filetoupload">
+ <input type="submit" value="Upload File" name="yourfilename">										                          </form>									 </body>
+ </html>
