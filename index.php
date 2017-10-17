de94d668266aef3465d0d67fa281b26f746dc823
Tiffany E Johnson unknown student <tej2@afsconnect2.njit.edu>
2017-10-17 18:08:15 -0400

    edit

--- index.php
+++ index.php
@@ -1,8 +1,4 @@
-
-<html>
-<link rel="stylesheet" href="styles.css"><body><h1>CSV Upload Form</h1><form
-action="index.php?page=uploadForm" method="post"
-enctype="multipart/form-data"><input type="file"
-name="fileToUpload" id="fileToUpload"><input type="submit"
-value="Upload File" name="submit"></form></body>
-</html>
+<html><link rel="stylesheet" href="styles.css"><body><h1>CSV Upload
+Form</h1><form method="post" enctype="multipart/form-data"><input type="file"
+name="fileToUpload" id="fileToUpload"><input type="submit" value="Upload Image"
+name="submit"></form> </body></html>
