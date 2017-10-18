874cc6de061811215d247a15de5ae518444ebfb3
Tiffany E Johnson unknown student <tej2@afsconnect2.njit.edu>
2017-10-17 23:06:50 -0400

    edits

--- index.php
+++ index.php
@@ -14,7 +14,7 @@ $obj = new main();
 
 class main {
 
-	public function _construct()
+	public function  __construct()
 	{
 		$requestPageType = 'homepage';
 		if(isset(REQUEST['page'])) {
@@ -29,4 +29,47 @@ class main {
 				}
 			}
 		}
-		?>
+abstract class page
+
+{
+protected $html;
+
+public function __construct()
+{
+        $this->html .= '<html>';
+	 $this->html .= '<link rel="stylesheet" href="styles.css">';
+	  }
+		public function __destruct()
+		{
+        $this->html .= '</html>';
+	 stringFunctions::printThis($this->html);
+		 }
+		public function get(){
+			echo 'Get method parent class';
+			}
+		public function post(){
+			echo 'Post method parent class';
+			}
+		}
+
+class homepage extends page
+
+        public function get()
+	        {
+		 $form = '<form method="post" enctype="multipart/form-data">';
+		 $form .= '<input type="file" name="fileToUpload" id="fileToUpload">';
+ 	         $form .= '<input type="submit"	value"Upload CSV File" name="submit">';		              $form .= '</form>';
+		 $this->html .= '<h1><b>Upload your CSV File Here!</h1></b>';
+		 $this->html .= $form;
+		 }
+	public function post()
+	{
+	$target_dir = "uploads/";
+	$target_file = str_replace(' ', '_', $target_dir . basename($FILES["fileToUpload"]["name"]));
+	$csvFileType = pathinfo($target_file,PATHINFO_EXTENSION);
+	$csvFileName = pathinfo($target_file,PATHINFO_BASENAME);
+	header('Location: index.php?page=DisplayWebTable&fileName='.$csvFileName);
+
+			
+			}
+			}
