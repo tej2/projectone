d50a0154b5f7d43321bc53b68701e9e978cdddc2
Tiffany E Johnson unknown student <tej2@afsconnect2.njit.edu>
2017-10-17 22:16:11 -0400

    edits

--- index.php
+++ index.php
@@ -1,21 +1,14 @@
 <?php
 
-//turn on the debugging messages
 ini_set('display_errors', 'On');
 error_reporting(E_ALL);
 
-//adding class to load classes finds the file when the program starts to
-fail for calling a missing class
-
 class Manage {
 	public static function autoload($class) {
-	//put any file name or directory here
 	include $class . '.php';
 	}
 }
-spl_autoload_register(array('Manage', 'autoload));
-
-//instantiate program object
+spl_autoload_register(array('Manage', 'autoload'));
 
 $obj = new main();
 
