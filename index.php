ec57b65aa8f9632234436d2ac034d8fdd21b871a
Tiffany E Johnson unknown student <tej2@afsconnect2.njit.edu>
2017-10-17 21:14:06 -0400

    final edit to index.php made

--- index.php
+++ index.php
@@ -1,4 +1,39 @@
-<html><link rel="stylesheet" href="styles.css"><body><h1>CSV Upload
-Form</h1><form method="post" enctype="multipart/form-data"><input type="file"
-name="fileToUpload" id="fileToUpload"><input type="submit" value="Upload Image"
-name="submit"></form> </body></html>
+<?php
+
+//turn on the debugging messages
+ini_set('display_errors', 'On');
+error_reporting(E_ALL);
+
+//adding class to load classes finds the file when the program starts to
+fail for calling a missing class
+
+class Manage {
+	public static function autoload($class) {
+	//put any file name or directory here
+	include $class . '.php';
+	}
+}
+spl_autoload_register(array('Manage', 'autoload));
+
+//instantiate program object
+
+$obj = new main();
+
+class main {
+
+	public function _construct()
+	{
+		$requestPageType = 'homepage';
+		if(isset(REQUEST['page'])) {
+			$requestPageType = $_REQUEST['page'];
+			}
+			$page = new $requestPageType;
+
+			if($_SERVER['REQUEST_METHOD'] == 'GET') {
+				$page->get();
+			} else {
+				$page->post();
+				}
+			}
+		}
+		?>
